47da24dd1b6b8f8cd55fbe00c1423af280c62e89
Анна Малышева <malyshevaanna000@gmail.com>
2026-09-09 09:47:21 +0300

    CatAPI added

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-# /cat/:tag/says/:text	Will return a random cat with a :tag and saying :text
-# https://cataas.com/cat/cute/says/hello
-
 # Название папки должно совпадать с названием вашей группы в Нетологии
 # Текст картинки также должен являться названием файла на Яндекс.Диске
 # Сохранить json файл с информацией о размере файла картинки в json-файл
@@ -19,39 +16,41 @@
 
 import requests
 
-class YandexDisk:             #Работает с API Яндекс-диска
-    def __init__(self, token):
-        pass
 
-    def create_folder(self, folder_name):
-        pass
+class CatAPI:
+    cat_base_url = "https://cataas.com/"
 
-    def get_upload_link(self, disk_path):
-        pass
-
-    def upload_file(self, local_path, disk_path):
-        pass
-
-    def file_exists(self, disk_path):
-        pass
-
-
-class CatAPI:    #Работает с API сайта с котами
     def get_cat_image(self, text):
-        pass
+        url = f'{self.cat_base_url}/cat/says/{text}'
+        response = requests.get(url)
+        return response
 
-    def save_image(self, image_data, filename):
-        pass
+
+# class YandexDisk:
+#     def __init__(self, token):
+#         pass
+#
+#     def create_folder(self, folder_name):
+#         pass
+#
+#     def get_upload_link(self, disk_path):
+#         pass
+#
+#     def upload_file(self, local_path, disk_path):
+#         pass
+
 
 
 def main():
-    cat_api = CatAPI()
-    yd = YandexDisk(token)
+    text = input("Введите текст для картинки: ")
 
-    image = cat_api.get_cat_image(text)
-    cat_api.save_image(image, filename)
-
-    yd.create_folder(folder_name)
-    yd.upload_file(filename, disk_path)
+    # cat_api = CatAPI()            #создали объект класса
+    # yd = YandexDisk(token)        #создали объект класса
+    #
+    # image = cat_api.get_cat_image(text)
+    # cat_api.save_image(image, filename)
+    #
+    # yd.create_folder(folder_name)
+    # yd.upload_file(filename, disk_path)
 
 main()
